0690efa625fac10db1bb908770669e251233f587
Kanat <110170587+NifeKKeen@users.noreply.github.com>
2026-03-03 14:30:03 +0500

    Week 6: AI made templates

--- Lab 6/kanichum/src/app/app.routes.ts
+++ Lab 6/kanichum/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AlbumsComponent } from './pages/albums/albums.component';
+import { AlbumDetailsComponent } from './pages/album-details/album-details.component';
+import { AlbumPhotosComponent } from './pages/album-photos/album-photos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'albums', component: AlbumsComponent },
+  { path: 'albums:id', component: AlbumDetailsComponent },
+  { path: 'albums:id/photos', component: AlbumPhotosComponent },
+];
